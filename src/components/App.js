4f7cf4b38b55b71c3cdd188c56b84570e7396363
vanshuhassija <vanshuhassija@gmail.com>
2022-02-15 21:43:15 +0530

    Completed Asignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,42 +1,33 @@
-import React, { Component, useState } from "react";
-import '../styles/App.css';
+import React, { Component, useEffect, useState } from "react";
+import "../styles/App.css";
 
-class App extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            renderBall: false,
-            posi : 0,
-            ballPosition: { left: "0px" }
-        };
-        this.renderChoice = this.renderBallOrButton.bind(this)
-        this.buttonClickHandler = this.buttonClickHandler.bind(this)
-    };
+function App() {
+  const [renderBall, setRenderBall] = useState(false);
+  const [position, setPosition] = useState(0);
+  const [ballPosition, setBallPosition] = useState({ left: 0 });
 
-    buttonClickHandler() {
-   
-   }
-    renderBallOrButton() {
-		if (this.state.renderBall) {
-		    return <div className="ball" style={this.state.ballPosition}></div>
-		} else {
-		    return <button onClick={this.buttonClickHandler} >Click For One Ball</button>
-		}
+
+  useEffect(() => {
+    document.addEventListener("keydown",function(event){
+        if(event.key==="ArrowRight"){
+            setBallPosition(prevState=>({left:prevState.left+5}))
+        }
+    })
+  }, []);
+
+  function buttonClickHandler() {
+     setRenderBall(true);
+  }
+
+  function renderBallOrButton() {
+    if (renderBall) {
+      return <div className="ball" style={{position:"absolute",left:ballPosition.left}}></div>;
+    } else {
+      return <button onClick={buttonClickHandler}>Click For One Ball</button>;
     }
+  }
 
-    // bind ArrowRight keydown event
-    componentDidMount() {
-      
-    }
-
-    render() {
-        return (
-            <div className="playground">
-                {this.renderBallOrButton()}
-            </div>
-        )
-    }
+  return <div className="playground">{renderBallOrButton()}</div>;
 }
-
 
 export default App;
